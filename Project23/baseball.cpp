25e5ec462a2205b8878d2ea265a1d7bee4f30562
shpark <zpxsch9001@naver.com>
2025-07-23 12:51:46 +0900

    [feature] page 27

--- Project23/baseball.cpp
+++ Project23/baseball.cpp
@@ -1,3 +1,10 @@
+#include <stdexcept>
+#include <string>
+
+using namespace std;
 class Baseball {
 public:
+	void guess(const string& string) {
+		throw length_error("Must be three letters.");
+	}
 };
--- Project23/baseball_test.cpp
+++ Project23/baseball_test.cpp
@@ -5,6 +5,11 @@ TEST(BaseballCame, TryGameTest) {
 	EXPECT_EQ(1, 1);
 }
 
+TEST(BaseballGame, ThrowexceptionWhenInputLengthIsUmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
