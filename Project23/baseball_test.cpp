ffdcf13a26c5bb7556677c83c2761dd125f15923
shpark <zpxsch9001@naver.com>
2025-07-23 13:02:52 +0900

    [refactoring] page 37

--- Project23/baseball_test.cpp
+++ Project23/baseball_test.cpp
@@ -1,18 +1,23 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
 
-TEST(BaseballCame, TryGameTest) {
-	EXPECT_EQ(1, 1);
-}
-
-TEST(BaseballGame, ThrowexceptionWhenInputLengthIsUmached) {
+class BaseballFixture : public testing::Test {
+public:
 	Baseball game;
-	EXPECT_THROW(game.guess(string("12")), length_error);
-}
+	void assertIllegalArgument(string guessNumber) {
+		try {
+			game.guess(string(guessNumber));
+			FAIL();
+		}
+		catch (exception e) {
 
-TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
-	Baseball game;
-	EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+		}
+	}
+};
+
+TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
+	assertIllegalArgument("12");
+	assertIllegalArgument("12s");
 }
 
 int main() {
